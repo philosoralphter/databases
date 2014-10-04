2d77ab0887bd1901b431c01e49e3c07315f4889c
RSamuel@ApolloII <theralphsamuel@gmail.com>
2014-10-03 17:44:02 -0700

    databases connected.  ready to work on tests.

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -4,6 +4,11 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  text VARCHAR(300),
+  user VARCHAR(30),
+  roomname VARCHAR(100),
+  ID int (10),
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
